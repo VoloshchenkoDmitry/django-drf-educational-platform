6ae871523fa5b41e835a0b3bf206167346f7b36b
VoloshchenkoDmitry <eldorado181118@gmail.com>
2025-10-09 22:44:44 +0300

    Добавлены Добавить модели LMS: курс, урок, регистрация

---
+++ materials/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    LessonListCreateAPIView,
+    LessonRetrieveAPIView,
+    LessonUpdateAPIView,
+    LessonDestroyAPIView
+)
+
+urlpatterns = [
+    path('', LessonListCreateAPIView.as_view(), name='lesson-list-create'),
+    path('<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
+    path('<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+]
