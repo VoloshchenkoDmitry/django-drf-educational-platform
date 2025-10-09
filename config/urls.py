e55fa14934518ddca97bfaa7c07559839105481d
VoloshchenkoDmitry <eldorado181118@gmail.com>
2025-10-09 22:44:44 +0300

    Добавлены Добавить модели LMS: курс, урок, регистрация

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from materials.views import CourseViewSet
+from users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/lessons/', include('materials.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
