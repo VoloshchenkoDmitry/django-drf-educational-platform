145f573bb836a7d522ece44f2826fc0ef05ebdb1
VoloshchenkoDmitry <eldorado181118@gmail.com>
2025-10-10 20:59:44 +0300

    Обновление моделей с полем владельца

--- materials/models.py
+++ materials/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from users.models import User
 
 
 class Course(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     preview = models.ImageField(upload_to='courses/previews/', blank=True, null=True, verbose_name='Превью')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='courses',
+        verbose_name='Владелец'
+    )
 
     class Meta:
         verbose_name = 'Курс'
@@ -24,6 +33,14 @@ class Lesson(models.Model):
         on_delete=models.CASCADE,
         related_name='lessons',
         verbose_name='Курс'
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='lessons',
+        verbose_name='Владелец'
     )
 
     class Meta:
