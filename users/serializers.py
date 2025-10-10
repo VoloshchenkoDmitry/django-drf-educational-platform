11686fe43c6469c9a64e65f94a0957383c8e2a38
VoloshchenkoDmitry <eldorado181118@gmail.com>
2025-10-10 20:56:04 +0300

    Настроены сериализаторы для пользователей

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,67 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth.password_validation import validate_password
+from .models import User, Payment
+from materials.models import Course, Lesson
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'password_confirm', 'first_name', 'last_name', 'phone', 'city')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    user_email = serializers.EmailField(source='user.email', read_only=True)
+    course_title = serializers.CharField(source='course.title', read_only=True)
+    lesson_title = serializers.CharField(source='lesson.title', read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
+class PaymentHistorySerializer(serializers.ModelSerializer):
+    course_title = serializers.CharField(source='course.title', read_only=True)
+    lesson_title = serializers.CharField(source='lesson.title', read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = ('id', 'payment_date', 'course', 'lesson', 'course_title', 'lesson_title',
+                  'amount', 'payment_method')
+        read_only_fields = fields
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    payment_history = PaymentHistorySerializer(many=True, read_only=True, source='payments')
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payment_history')
+        read_only_fields = ('id',)
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar')
-        read_only_fields = ('id',)
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'is_staff')
+        read_only_fields = ('id', 'is_staff')
+
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'city', 'avatar')
+        read_only_fields = fields
